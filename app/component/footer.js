aa55614c7ed59335132767f97d4c5c0c255f067c
sahil sinha <sahilsinha2204@gmail.com>
2024-11-25 18:05:40 +0530

    opld

---
+++ app/component/footer.js
@@ -0,0 +1,84 @@
+"use client"
+import React from 'react';
+import Link from 'next/link';
+import Insta from "@/public/Insta.svg";
+import Linkedin from "@/public/Linkdinn.svg";
+import Twitter from "@/public/Twit.svg";
+import Youtube from "@/public/Yout.svg";
+import Image from 'next/image';
+import Logo from "@/public/logo.png"
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-card-foreground pb-8 md:px-12 md:pb-12">
+    <div className=" border-t  border-[#535353] ">
+      <div className="flex-1 md:flex  px-10 pt-20   pb-12 justify-between">
+        <div className="w-full md:w-2/4 mb-6 Poppins md:mb-0 sm:pr-2 lg:pr-0 ">
+          <div className="flex w-full items-center mb-2">
+            <Link href="/">
+            <Image src={Logo} alt="Techlerate Logo" className="" width={220} height={20}/>
+            </Link>
+          </div>
+          <p className="text-[#7b61ff]  ">Connect with us:</p>
+          <p className="text-[#9a9898] ">Call: +91 88 820 76 755</p>
+          <p className="text-[#9a9898] ">Email: contact@techlerate.com</p>
+          <p className="text-[#9a9898] w-auto lg:w-96">Office: Platina Heights, C24, C Block, Phase 2,Industrial Area, Sector 62, Noida, Uttar Pradesh 201309</p>
+          <div className="flex mt-6 space-x-4 ">
+            <Link href="#" onClick={(e) => e.preventDefault()}  aria-label="Instagram">
+              <Image src={Insta} alt="Instagram Icon" className='rounded-full' width={40} height={2}/>
+            </Link>
+            <Link href="#" onClick={(e) => e.preventDefault()} aria-label="LinkedIn">
+              <Image src={Linkedin} alt="LinkedIn Icon" className='rounded-full' width={40}  height={2}/>
+            </Link>
+            <Link href="#" onClick={(e) => e.preventDefault()} aria-label="Twitter">
+              <Image src={Twitter} alt="Twitter Icon" className='rounded-full ' width={40}  height={2}/>
+            </Link>
+            <Link href="#" onClick={(e) => e.preventDefault()} aria-label="YouTube">
+              <Image src={Youtube} alt="YouTube Icon" className='rounded-full ' width={40}  height={2}/>
+            </Link>
+          </div>
+        </div>
+  
+        <div className="w-full md:w-2/4 flex flex-row">
+          <div className="w-full sm:w-1/3 mb-6 sm:mb-0">
+            <h3 className=" text-white mb-4">Solution</h3>
+            <ul className="space-y-2">
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Brand & Design</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Video Production</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Social Media</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Digital Campaign</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">UI/UX</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">SEO</Link></li>
+            </ul>
+          </div>
+          <div className="w-full sm:w-1/3 mb-6 sm:mb-0">
+            <h3 className=" text-white mb-4">Company</h3>
+            <ul className="space-y-2">
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">About</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Contact</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Testimonial</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Partners</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Portfolio</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Careers</Link></li>
+            </ul>
+          </div>
+          <div className="w-full sm:w-1/3">
+            <h3 className=" text-white mb-4">Support</h3>
+            <ul className="space-y-2">
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Help center</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Terms of service</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Legal</Link></li>
+              <li><Link href="#" onClick={(e) => e.preventDefault()} className="text-white hover:text-primary">Privacy policy</Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="mt-8 border-t border-[#535353] pt-8 px-8 pb-10 text-center">
+        <p className="text-[#9a9898] text-sm">&copy;2024 HotelBarsana. All rights reserved</p>
+      </div>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer
---
+++ app/component/navbar.js
@@ -0,0 +1,263 @@
+"use client";
+import { usePathname } from 'next/navigation'
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.png";
+import Star from "@/public/Star.svg";
+import Starx from "@/public/Starx.svg"
+import Arrow from "@/public/Arrow.png"
+import { useRouter } from 'next/navigation';
+import ServicesPic from "@/public/ServicesPic.png";
+import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
+
+const services = [
+
+  { name: "On Demand", logo: "/OnDemand.svg" },
+  { name: "eCommerce", logo: "/Ecommerce.svg" },
+  { name: "Real Estate", logo: "/Realestate.svg" },
+  { name: "Healthcare", logo: "/Healthcare.svg" },
+  { name: "SaaS", logo: "/Saas.svg" },
+  { name: "Education", logo: "/Education.svg" },
+  { name: "Restaurant", logo: "/Restaurant.svg" },
+  { name: "Games", logo: "/Games.svg" },
+  { name: "News", logo: "/News.svg" },
+  { name: "Entertainment", logo: "/Entertainment.svg" },
+  { name: "Wellness", logo: "/Wellness.svg" },
+  { name: "Logistics", logo: "/Logistics.svg" },
+  { name: "Travel", logo: "/Travel.svg" },
+  { name: "Finance", logo: "/Finance.svg" },
+  { name: "Aviation", logo: "/Travel.svg" },
+  { name: "E-Scooter", logo: "/Escooter.svg" },
+  { name: "Politics", logo: "/Politics.svg" },
+  { name: "Agriculture", logo: "/Agriculture.svg" },
+  { name: "Events", logo: "/Events.svg" },
+  { name: "Social Networking", logo: "/SocialN.svg" },
+  { name: "EV", logo: "/EV.svg" },
+  { name: "Oil and Gas", logo: "/Oil&gas.svg" },
+  { name: "Banking", logo: "/Banking.svg" },
+  { name: "Automotive", logo: "/Automotive.svg" },
+  { name: "Telecom", logo: "/Telecom.svg" },
+  { name: "Insurance", logo: "/Insurance.svg" },
+  { name: "Manufacturing", logo: "/Manufacturing.svg" },
+
+  { name: "Construction", logo: "/Construction.svg" },
+
+
+];
+const technology = [
+
+  { name: "React Native", logo: "/Technology/reactNative.svg", link:"/Technology/reactnative" },
+  { name: "AI Development", logo: "/Technology/Ai.svg", link:"#" },
+  { name: "Chatbot", logo: "/Technology/Chatbot.svg", link:"/Technology/Chatbot" },
+  { name: "IOT", logo: "/Technology/IOT.svg", link:"#" },
+  { name: "Augmented Reality", logo: "/Technology/AR.svg", link:"#" },
+  { name: "Ionic", logo: "/Technology/Ionic.svg", link:"#" },
+  { name: "VR Development", logo: "/Technology/VR.svg", link:"#" },
+  { name: "PHP/Nodes JS", logo: "/Technology/Php.svg", link:"#" },
+  { name: "Quality Analyst", logo: "/Technology/Quality.svg", link:"#" },
+  { name: "Wearable", logo: "/Technology/Wearable.svg", link:"#" },
+  { name: "iOS App", logo: "/Technology/IOS.svg", link:"#" },
+  { name: "Flutter", logo: "/Technology/Flutter.svg", link:"/Technology/flutter" },
+  { name: "Android App", logo: "/Technology/Android.svg", link:"#" },
+  { name: "Blockchain", logo: "/Technology/Blockchain.svg", link:"#" },
+  { name: "UI/UX Designing", logo: "/Technology/Ui.svg", link:"#" },
+
+
+];
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  console.log(router.pathname);
+  const pathname = usePathname()
+ 
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [isOpenMenuT, setIsOpenMenuT] = useState(false);
+
+  
+  const isContactPage = router.pathname === '/contact';
+
+  return (
+    <div className={`${pathname == "/Technology/Chatbot" || "/Technology/flutter" ? 'bg-[#18191F]':'bg-black'} `}>
+    <nav className={`  ${pathname == "/Technology/Chatbot" || "/Technology/flutter" ? 'bg-[#18191F]':'bg-black'} items-center  p-4 px-4   2xl:px-20  lg:p-4  `}>
+      <div className=" relative xl:px-10  flex items-center justify-between lg:justify-around flex-wrap">
+        <div className="flex items-center flex-shrink-0 text-white">
+       <Link href="/"> <Image
+            src={Logo}
+            alt="Techlerate Logo"
+            className="h-16 w-48 md:w-96  md:h-14 lg:h-10 xl:h-20 lg:w-60 2xl:w-76"
+            width={400}
+            height={5}
+          />
+          </Link>  
+        </div>
+        <div className="block lg:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="flex items-center pr-3 py-2 font-extrabold rounded text-zinc-500 hover:text-black-400"
+          >
+            <svg
+              className={`fill-current h-5 sm:h-10 sm:w-10 w-5 ${isOpen ? "hidden" : "block"
+                }`}
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+            <svg
+              className={`fill-current h-5 w-5 ${isOpen ? "block" : "hidden"}`}
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`w-full block flex-grow justify-center text-center lg:flex text-lg lg:items-center lg:w-auto ${isOpen ? "block" : "hidden"
+            }`}
+        >
+          <div className="text-lg 2xl:text-xl justify-around text-center lg:flex-grow">
+            <Link
+              href="/About"
+              className={`nav-link block  mt-4 lg:inline-block lg:mt-0 text-white-200 mr-8 ${pathname== "/About"?"text-[#7B61FF]":"text-white hover:text-[#7B61FF]"}`}
+            >
+              About
+            </Link>
+            <Link
+              href="/Work"
+              className={`nav-link block  mt-4 lg:inline-block lg:mt-0 text-white-200 mr-8 ${pathname== "/Work"?"text-[#7B61FF]":"text-white hover:text-[#7B61FF]"}`}
+            >
+              Work
+            </Link>
+            <div className=" lg:inline-block  group"
+               onMouseEnter={() => setIsOpenMenu(true)}
+               onMouseLeave={() => setIsOpenMenu(false)}>
+              <Link
+                href="#"
+                className={`nav-link block  mt-4 lg:inline-block group lg:mt-0 hover:text-[#7B61FF] mr-8 ${pathname== "/Services"?"text-white hover:text-[#7B61FF]":"text-white"}`}
+                aria-haspopup="true"
+                aria-expanded={isOpen ? true : false}
+              >
+                Services
+                {isOpenMenu ? (
+                  <FaChevronUp className="inline-flex ml-2" size={14} />
+                ) : (
+                  <FaChevronDown className="inline-flex ml-2" size={14} />
+                )}              </Link>
+              <div 
+                className={`absolute w-full left-0 p-2 md:p-6 mt-12 lg:mt-10   h-auto md:h-auto    Glassy rounded-md z-50 opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-all duration-300
+                   ${pathname ==="/Technology/reactnative" || pathname === "/Work/LiquiClear"  ? 'text-[#000000]' : 'text-white'}`}
+              >
+                <section className="flex flex-col md:flex-row justify-between  rounded-3xl w-full  md:gap-4 lg:gap-6 xl:gap-8 2xl:gap-10  pt-14 md:pt-16 lg:pt-4 pb-6 md:px-0 md:flex Poppinx">
+                  <div className="p-6 xl:p-10">
+
+
+                    <div className=" w-auto text-2xl md:text-xl lg:text-3xl  text-left xl:text-3xl 2xl:text-4xl  ">
+
+                      <p className="leading-relaxed text-left">
+                        60
+                        <div className="inline-block ml-1 align-middle">
+                          <Image src={Starx} alt="star" className="w-auto" width={28} height={28} />
+                        </div>
+                        &nbsp;Software Types <br /> in Our Development <br /> Portfolio
+                      </p>
+
+                    </div>
+                    <div className={`mt-5 text-left block  `}>
+                      <Image src={ServicesPic} className=" w-72 lg:w-auto xl:w-full" alt="" width={400} height={400} />
+                      <h2 className="mt-5 text-sm md:text-xs lg:text-lg  2xl:text-2xl leading-relaxed font-light">Read our developments that has helped <br />the industry boom in India </h2>
+                    </div>
+                  </div>
+                  <ul className="py-1 text-xs  lg:text-base xl:text-lg Poppin grid grid-cols-3">
+                    {services.map((service, index) => (
+                      <li key={index} className={`flex items-center mr-2 dropslash lg:mr-2 xl:mr-10   w-auto sm:w-[220px] md:w-full lg:w-[200px]  xl:max-w-7xl xl:w-[250px] hover:text-[#7b61ff] custom-filter `}>
+                        <Image
+                          src={service.logo}
+                          alt={`${service.name} logo`}
+                          className="lg:h-8 lg:w-8 h-6 w-6 mr-1 md:mr-2 xl:h-8 xl:w-8 2xl:w-10 2xl:h-10"
+                          width={100}
+                          height={100}
+                        />
+                        <Link
+                         /* href={`/services/${service.name.toLowerCase().replace(/\s+/g, "")}`}*/
+                         href="#"
+                          className="block lg:px-2 py-2 md:py-4 lg:py-0 "
+                        >
+                          {service.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+              </div>
+            </div>
+            <div className="lg:inline-block group"
+               onMouseEnter={() => setIsOpenMenuT(true)}
+               onMouseLeave={() => setIsOpenMenuT(false)}>
+      <Link
+        href="#"
+        className={`block nav-link mt-4 lg:inline-block lg:mt-0 hover:text-[#7B61FF] mr-8 ${pathname== "/Technology/reactnative"?"text-[#7B61FF]":"text-white"} ${
+        router.pathname ? 'text-[#7b61ff]' : 'text-white'
+        }`}
+      >
+        Technology
+        {isOpenMenuT ? (
+                  <FaChevronUp className="inline-flex ml-2" size={14} />
+                ) : (
+                  <FaChevronDown className="inline-flex ml-2" size={14} />
+                )}      </Link>
+      <div
+        className={`absolute w-full  lg:left-4  m-auto  justify-center mt-10 h-auto md:h-auto Glassy z-50 opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-all duration-300 ${
+         pathname ==="/Technology/reactnative" || pathname === "/Work/LiquiClear"  ? 'text-[#000000]' : 'text-white'
+        }`}
+      >
+        <div className="flex flex-col md:flex-row justify-center  lg:justify-between  rounded-3xl w-full  md:gap-6 lg:gap-6 xl:gap-8 2xl:gap-10 pt-14 md:pt-10 lg:pt-16 pb-6 md:px-0 md:flex Poppinx">
+          <div className="w-auto mt-6 lg:mt-0">
+            <h2 className="text-xl lg:text-3xl  xl:text-4xl text-left font-medium pl-6 Poppind">Technologies</h2>
+            <Image src="/Technology/Technologyimg.png" className="w-72 m-4  md:w-72 xl:w-[400px] " alt="" width={400} height={400} />
+          </div>
+          <div className="py-1  text-xs sm:text-xs lg:text-base xl:text-lg gap-6  w-auto Poppin grid grid-cols-2 sm:grid-cols-3">
+            {technology.map((service, index) => (
+              <div
+                key={index}
+                className={` dropslash  inline-flex  items-center w-auto sm:w-[200px] md:w-[120px] lg:w-[200px] xl:w-[250px] 2xl:w-[300px]  hover:text-[#7b61ff] `}
+              >
+                <div className="lg:h-8 lg:w-8 h-6 w-6 sm:h-6 sm:w-6 hover:bg-yellow-700 hover-dropslash p-1 lg:p-1.5 2xl:p-2 bg-gray-700 rounded-full md:h-6 md:w-6 mr-1 md:mr-2 xl:h-8 xl:w-8 2xl:w-8 2xl:h-8">
+                  <Image src={service.logo} alt={`${service.name} logo`} width={100} height={100} />
+                </div>
+                <Link href={`${service.link}`} className={`block lg:px-2 whitespace-nowrap overflow-hidden text-ellipsis py-2 md:py-4 lg:py-0`}>
+                  {service.name}
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+            <Link
+              href="/Career"
+              className=" block nav-link text-white mt-4 lg:inline-block hover:text-[#7B61FF] lg:mt-0 text-white-200 mr-8"
+            >
+              Career
+            </Link>
+          </div>
+         
+        </div>
+        <div className={` text-lg 2xl:text-xl  w-auto m-auto items-center lg:flex justify-center  lg:justify-normal  text-center ${isOpen ? "block" : "hidden"
+                }`}>
+            < Link href="/contact" className="inline-flex">
+              <button className="block nav-link text-white hover:text-[#7B61FF] mt-4  lg:mt-0 lg:inline-block text-white-200 mr-2 xl:mr-4">
+                Contact Us
+              </button>
+              <Image src={Star} alt="" className="h-6 w-6 mt-4  lg:mt-0" width={20} height={20} />
+            </Link>
+          </div>
+      </div>
+    </nav>
+    </div>
+  );
+};
+
+export default Navbar;
